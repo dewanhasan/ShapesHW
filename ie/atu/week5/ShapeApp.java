387b85ce5e8a3c6d3fe4e84426f649b2ee3423d0
Dewan Hasan <g00410156@atu.ie>
2022-10-18 13:54:37 +0100

    Promt user for length

--- ie/atu/week5/ShapeApp.java
+++ ie/atu/week5/ShapeApp.java
@@ -1,4 +1,12 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class ShapeApp {
+    public static void main(String[] args) {
+        System.out.println("Please Enter the width of the rectangle");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+        System.out.println("You Entered : " + length);
+    }
 }
