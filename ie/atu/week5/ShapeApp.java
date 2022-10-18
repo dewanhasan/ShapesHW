83a69fcb22a8e20b0a1fb99d6ef357f5d7f503a3
Dewan Hasan <g00410156@atu.ie>
2022-10-18 14:12:35 +0100

    Promt user for width

--- ie/atu/week5/ShapeApp.java
+++ ie/atu/week5/ShapeApp.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class ShapeApp {
     public static void main(String[] args) {
-        System.out.println("Please Enter the width of the rectangle");
+        System.out.println("Please Enter the length of the rectangle");
         Scanner input = new Scanner(System.in);
         double length = input.nextDouble();
         System.out.println("You Entered : " + length);
+
+        System.out.println("Please Enter the width of the rectangle");
+        double width = input.nextDouble();
+        System.out.println("You Entered : " + width);
     }
 }
